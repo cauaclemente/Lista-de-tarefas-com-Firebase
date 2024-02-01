af22b6aa2098307f15a0867d20684dc4f0942737
Cauã Clemente <cauaclemente07@gmail.com>
2024-02-01 20:16:33 -0300

    Privando a rota Admin

--- testes-react/src/Routes/Routes.jsx
+++ testes-react/src/Routes/Routes.jsx
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../Pages/home/Home";
 import Register from "../Pages/Register/Register";
 import Admin from "../Pages/Admin/Admin"
+import Private from "./Private";
 
 function RoutesApp() {
     return (
         <Routes>
             <Route path="/" element={ <Home /> } />
             <Route path="/register" element={ <Register /> } />
-            <Route path="/Admin" element={ <Admin />} />
+            <Route path="/Admin" element={ <Private> <Admin /> </Private>} />
         </Routes>
     )
 }
