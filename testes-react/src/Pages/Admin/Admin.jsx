3c832de6913ac1f1e00b95331563faae5a37ee00
Cauã Clemente <cauaclemente07@gmail.com>
2024-02-05 18:13:20 -0300

    buscando dados do firebase

--- testes-react/src/Pages/Admin/Admin.jsx
+++ testes-react/src/Pages/Admin/Admin.jsx
@@ -1,22 +1,80 @@
 import "./admin.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
-import { auth } from "../../Firebase"
+import { auth, db } from "../../Firebase"
 import { signOut } from "firebase/auth"
+import {
+    addDoc,
+    collection,
+    onSnapshot,
+    query,
+    orderBy,
+    where
+} from "firebase/firestore"
 
 const Admin = () => {
   
     const [tarefaInput, setTarefaInput] = useState("")
+    const [user, setUser] = useState({})
 
-    function handleRegister(e) {
+    const [tarefas, setTarefas] = useState([])
+
+    useEffect(() => {
+        async function loadTarefas(){
+            const userDetail = localStorage.getItem("@detailUser")
+            setUser(JSON.parse(userDetail))
+
+        if(userDetail) {
+            const data = JSON.parse(userDetail);
+
+            const tarefaref = collection(db, "tarefas")
+            const q = query(tarefaref, orderBy("created", "desc"), where("userUid", "==", data?.uid))
+
+            const unsub = onSnapshot(q , (snapshot) => {
+                let lista = []
+
+                snapshot.forEach((doc) => {
+                    lista.push({
+                        id: doc.id,
+                        tarefa: doc.data().tarefa,
+                        userUid: doc.data().userUid
+                    })
+                })
+                console.log(lista);
+                setTarefas(lista)
+            })
+        }
+        }
+    
+        loadTarefas();
+      }, [])
+    
+      async function handleRegister(e){
         e.preventDefault();
-        alert("clicou")
-    }
-
-    async function handleLogout() {
-        await signOut(auth)
-    }
-
+    
+        if(tarefaInput === ''){
+          alert("Digite sua tarefa...")
+          return;
+        }
+    
+        await addDoc(collection(db, "tarefas"), {
+            tarefa: tarefaInput,
+            created: new Date(),
+            userUid: user?.uid
+        })
+        .then(() => {
+            console.log("TAREFA REGISTRADA")
+            setTarefaInput('')
+        })
+        .catch((error) => {
+            console.log("ERRO AO REGISTRAR " + error)
+        })    
+    
+      }
+    
+      async function handleLogout(){
+        await signOut(auth);
+      }
 
     return (
     <div className="adm-container">
@@ -31,13 +89,15 @@ const Admin = () => {
             <button className="btn-register" type="submit">Registrar tarefa</button>
         </form>
 
-        <article className="list">
-            <p>Estudar hj a noite</p>
+        {tarefas.map((item) => (
+            <article key={item.id} className="list">
+            <p>{item.tarefa}</p>
             <div>
                 <button>Editar</button>
                 <button className="btn-delete">Concluir</button>
             </div>
         </article>
+        ))}
 
         <button className="btn-logout" onClick={handleLogout}>Sair</button>
 
